6b6f7f971fc513e04107f105178570ebf4330d32
ngo275 <ngo38114@gmail.com>
2018-04-02 23:34:23 +0900

    installed material-ui-next and propTypes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import intl from 'react-intl-universal'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Button from 'material-ui/Button'
 
-export class Navbar extends Component {
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+})
+
+class Navbar extends Component {
   render () {
     return (
-      <div>hoge</div>
+      <div>
+        <Button className={this.props.classes.button} onClick={this.handleLangButton}>
+          {intl.get('languages')}
+        </Button>
+      </div>
     )
+  }
+
+  handleLangButton = () => {
   }
 }
 
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Navbar)
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,25 @@
+// @flow
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import intl from 'react-intl-universal'
+import { CircularProgress } from 'material-ui/Progress'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { withStyles } from 'material-ui/styles'
 import { initialize, setLocale } from '../ducks/intl'
 import type { Locale } from '../types'
+import Navbar from '../components/Navbar'
+
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
+  centered: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+})
 
 class App extends Component {
 
@@ -12,17 +29,33 @@ class App extends Component {
   }
 
   render() {
-    // this.props.initLocale()
-    console.log(this.props.intl)
-    // const { intl } = this.props
+    const isLoading = this.props.intl === undefined || this.props.intl.isLoading
+
     return (
-      <div className="App">
-        <header className="App-header">
-        </header>
+      <div className='App'>
+        <MuiThemeProvider>
+          { isLoading ? this._renderLoading() : this._renderContents() }
+        </MuiThemeProvider>
+      </div>
+    )
+  }
+
+  _renderContents = () => {
+    return (
+      <div>
+        <Navbar />
         <p className="App-intro">
           {intl.get('en-US')}
           hogehoge
         </p>
+      </div>
+    )
+  }
+
+  _renderLoading = () => {
+    return (
+      <div className={this.props.classes.centered}>
+        <CircularProgress className={this.props.classes.progress} size={50} />
       </div>
     )
   }
@@ -41,4 +74,8 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App))
